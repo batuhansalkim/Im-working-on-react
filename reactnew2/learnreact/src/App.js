c2c46ab1575b76db5a42d2fcef9cc8672e5194ec
Baturpcu <batusalkim11@gmail.com>
2022-12-20 16:59:29 +0300

    update

--- reactnew2/learnreact/src/App.js
+++ reactnew2/learnreact/src/App.js
@@ -5,6 +5,7 @@ import Profile from './components/Profile';
 function App() {
   return (
     <div className="App">
+      <h1>deneme sürümü yükleniyor...</h1>
       <Profile name="batuhax"/>
     </div>
   );
--- reactnew2/learnreact/src/components/Profile.js
+++ reactnew2/learnreact/src/components/Profile.js
@@ -1,6 +1,7 @@
 function Profile(props){
     console.log(props);
     return <h1>Name : {props.name}</h1>
+    
 }
 
 export default Profile;
