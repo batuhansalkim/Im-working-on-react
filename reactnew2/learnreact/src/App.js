c46721cb7c8bdd4e9d3fc1bd1f593c02a4418d09
Baturpcu <batusalkim11@gmail.com>
2022-12-20 17:05:28 +0300

    last test

--- reactnew2/learnreact/src/App.js
+++ reactnew2/learnreact/src/App.js
@@ -5,7 +5,7 @@ import Profile from './components/Profile';
 function App() {
   return (
     <div className="App">
-      
+      <h4>LAST TEST DATE : 20.12.2022</h4>
       <Profile name="batuhax"/>
     </div>
   );
