c088ae180a4156dea39a7b1a0c73f90706aeca69
Baturpcu <batusalkim11@gmail.com>
2023-02-18 23:17:18 +0300

    update

--- reactnew2/learnreact/src/App.js
+++ reactnew2/learnreact/src/App.js
@@ -1,31 +1,9 @@
-import './App.css';
-import './scss/FileName.scss';
-import ''
-import './tailwind.css';
-import Button from './components/Button';
-import Tab from './components/Tab';
+import React from "react";
 
-function App() {
- 
-  return (
-    <>
-
-    <div style={{padding:20}}>
-      <Tab activeTab={1}>
-        <Tab.Panel title="profil">1. tab</Tab.Panel>
-          <Tab.Panel title="Hakkında">2. tab</Tab.Panel>
-          <Tab.Panel title="Ayarlar">3. tab</Tab.Panel>
-      </Tab>
-    </div>
-    <div style={{padding:20}}>
-      <Button text="buton örnegi" />
-      <Button text="buton örnegi" variant="success"/>
-      <Button text="buton örnegi" variant="danger"/>
-      <Button text="buton örnegi" variant="warning"/>
-    </div>
-
-    </>
-  );
+function App(){
+  return(
+    <div className="app">app</div>
+  )
 }
 
 export default App;
