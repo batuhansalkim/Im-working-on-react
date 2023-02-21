c8faeaa8a439dae981bb90d29dcf00c87250566d
Baturpcu <batusalkim11@gmail.com>
2023-02-21 22:02:21 +0300

    update

--- ReactProject/my-app-project/src/App.js
+++ ReactProject/my-app-project/src/App.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
 import "./App.css";
-import Navbar from './components/Navbar';
-import Users from './components/Users';
+
 export default class App extends Component {
   
   render() {
     return (
       <div className='container'>
-        <Navbar title='User App'/>
-        <hr />
-        <Users users={this.state.users}/>
+        <h1>batuhan</h1>
       </div>
     );
   }
