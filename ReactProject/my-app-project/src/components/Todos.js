1ce211e0d6e76ae86ee3c430edb17f3f8e8da2b0
Baturpcu <batusalkim11@gmail.com>
2023-02-21 23:29:49 +0300

    update

--- ReactProject/my-app-project/src/components/Todos.js
+++ ReactProject/my-app-project/src/components/Todos.js
@@ -3,7 +3,13 @@ import React from "react";
 export default function Todos(){
     return(
         <ul className="todos">
-            <li className="todos__item"></li>
+            <li className="todos__item">
+                <span className="todos__text">GYM</span>
+                <div className="todos__buttons">
+                    <a href="#">✅</a>
+                    <a href="#">❌</a>
+                </div>
+            </li>
         </ul>
     )
 }
