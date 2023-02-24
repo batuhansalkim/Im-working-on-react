864d1185dc6997d9fb9a66a0d209bc951c03dbbf
Baturpcu <batusalkim11@gmail.com>
2023-02-25 02:00:34 +0300

    update

--- ReactProject/learnreact/src/App.js
+++ ReactProject/learnreact/src/App.js
@@ -2,30 +2,32 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 function App(){
+
   const [todos,setTodos] = useState([]);
+
   useEffect(()=>{
-    const fetchTodos = async ()=>{
-      try{
-        const response = await axios.get("https://jsonplaceholder.typicode.com/todos/");
-        setTodos(response.data);
-      }catch(err){
-        console.log(err);
-      }
+  const fetchTodos = async ()=>{
+    try{
+      const response = await axios.get("https://jsonplaceholder.typicode.com/todos/");
+      setTodos(response.data);
+    }catch(err){
+      console.log(err);
     }
-    fetchTodos();
+  }
+  fetchTodos();
   },[])
+
   return(
     <>
     <div className="todo-wrapper">
       <div className="todo">
-        <h3 className="todo-title">Deneme</h3>
-        <input type="checked" checked  className="todo-completed"/>
+        <div className="todo-title">asdsa</div>
+        <input type="checkbox" checked className="todo-completed" />
       </div>
     </div>
     </>
   )
 }
-
 export default App;
 
 
