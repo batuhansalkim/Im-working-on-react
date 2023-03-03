cb3ba9f7443f23b6e69fabbdfbbc0e985af776e8
Baturpcu <batusalkim11@gmail.com>
2023-03-04 01:36:50 +0300

    update

--- ReactProject/lastreactproject/batu-app/src/App.js
+++ ReactProject/lastreactproject/batu-app/src/App.js
@@ -1,17 +1,24 @@
 import './App.css';
-import React, {useState,useEffect} from "react";
+import React, {useState,useRef} from "react";
 
 function App() {
 
-  const [status,setStatus]  = useState("false");
+  const [count,setCount]  = useState(0);  
+  const firstRef = useRef(0);
 
-  useEffect(()=>{
-    setStatus("deneme2")
-  },50000)
+  const func = (num)=>{
 
+  }
+
+  console.log("firstRef",firstRef.current.value = 7);
+
+  const refFunc = ()=>{
+   // firstRef.current
+  }
   return(
     <div>
-      {status}
+      <div>{status}</div>
+      <input onClick={refFunc} ref={firstRef} placeholder='ara' />
     </div>
   )
  
@@ -19,3 +26,4 @@ function App() {
 }
 
 export default App;
+//1:14 de kaldım
