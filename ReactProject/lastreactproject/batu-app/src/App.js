e8cea202885a17a9718c032a5e39308f645cd31e
Baturpcu <batusalkim11@gmail.com>
2023-03-03 18:26:33 +0300

    update

--- ReactProject/lastreactproject/batu-app/src/App.js
+++ ReactProject/lastreactproject/batu-app/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import React, {useState,useEffect} from "react";
 
 function App() {
 
-  return (
-    <div>
+  const [count,setCount]  = useState(10);
+  
+  useEffect(()=>{
+    document.title = `you clicked ${count} times `;
+  });
 
+
+
+  return(
+    <div>
+      <p>you clicked {count} times</p>
+      <button onClick={()=>setCount(count+1)}>click me</button>      
     </div>
   )
 }
