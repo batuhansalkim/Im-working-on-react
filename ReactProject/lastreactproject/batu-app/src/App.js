7835eacfb0db9c899c00898938c127f4335c7bd6
Baturpcu <batusalkim11@gmail.com>
2023-03-07 19:43:21 +0300

    update

--- ReactProject/lastreactproject/batu-app/src/App.js
+++ ReactProject/lastreactproject/batu-app/src/App.js
@@ -1,14 +1,15 @@
-import './App.css';
+import styles from './App.module.css';
 import React from "react";
 
 function App() {
- 
-
+ const age = 16;
   return(
     <div>
-      <h1>batuhan</h1>
+      {age>=18 ? <h1>Over Age</h1> : <h1>Under Age</h1>}
     </div>
   )
-}
+ }
+  
+
 
 export default App;
