1034343958eba4ea1ae26d229b4209355ce9e08d
Baturpcu <batusalkim11@gmail.com>
2023-03-13 18:13:55 +0300

    update

--- ReactProject/lastreactproject/todo-app/batu-todoapp/src/components/Form.js
+++ ReactProject/lastreactproject/todo-app/batu-todoapp/src/components/Form.js
@@ -1,16 +1,25 @@
 import {useForm} from 'react-hook-form';
+import * as yup from 'yup';
+
 const Form = ()=>{
     const {register, handleSubmit} = useForm();
 
-    const onSubmit = ()=>{
-        console.log("HELLO WORLD");
+    const schema = yup.object().shape({
+        fullName: yup.string().required(),
+        
+    })
+
+    const onSubmit = (data)=>{
+        console.log(data);
+        console.log(data.fullName);
+        console.log(data.email);
     }
 
     return(
         <form onSubmit={handleSubmit(onSubmit)}>
             <input type="text" placeholder="Full Name..." {...register("fullName")}/>
             <input type="text" placeholder="Email..." {...register("email")} />
-            <input type="text" placeholder="Age..." {...register("age")} />
+            <input type="number" placeholder="Age..." {...register("age")} />
             <input type="password" placeholder="Password..." {...register("password")} />
             <input type="password" placeholder="Confirm Password..." {...register("confirmPassword")} />
             <input type="submit" />
