de6455d24baae0dddc7e1b5b7ec8c65055e0d5c2
Baturpcu <batusalkim11@gmail.com>
2023-03-16 02:32:59 +0300

    update

--- ReactProject/lastreactproject/todo-app/batu-todoapp/src/App.js
+++ ReactProject/lastreactproject/todo-app/batu-todoapp/src/App.js
@@ -1,16 +1,12 @@
-
 import './App.css';
-import { BatuhanSalkim } from './useCount';
 
 
 function App() {
-  const {count,arttır,azalt,reset} = BatuhanSalkim();
+
   return (
     <div className='App'>
       
-      <button onClick={arttır}>arttır</button>    
-      <button onClick={azalt}>azalat</button>    
-      <button onClick={reset}>sıfırla</button>    
+          
     </div>
     
   );
