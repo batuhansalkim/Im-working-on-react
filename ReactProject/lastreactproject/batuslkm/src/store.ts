187449b6d6c6ecfb8f3c03459ae1628f987a4118
Baturpcu <batusalkim11@gmail.com>
2023-03-17 00:22:15 +0300

    update

--- ReactProject/lastreactproject/batuslkm/src/store.ts
+++ ReactProject/lastreactproject/batuslkm/src/store.ts
@@ -1,4 +1,23 @@
-import {configureStore} from "@reduxjs/toolkit";
+import {configureStore,createSlice} from "@reduxjs/toolkit";
 
+const initialState = {value:{username:""}};
+const userSlice = createSlice({
+    name:"user",
+    initialState,
+    reducers:{
+        login:(state,action)=>{
+            state.value = action.payload;
+        },
+        logout: (state)=>{
+            state = initialState;
+        },
+    },
+});
 
-export const store =  configureStore({})
+export const {login,logout} = userSlice.actions;
+
+export const store =  configureStore({
+    reducer:{
+        user:userSlice.reducer,
+    }
+})
