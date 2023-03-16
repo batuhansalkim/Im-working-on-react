98a7563eb72d9d15ae0a75f5a20d1110411c0252
Baturpcu <batusalkim11@gmail.com>
2023-03-16 11:04:14 +0300

    update

--- ReactProject/lastreactproject/todo-app/batu-todoapp/src/App.js
+++ ReactProject/lastreactproject/todo-app/batu-todoapp/src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
-
+import { Person } from './components/Person';
 
 function App() {
 
   return (
     <div className='App'>
-      
+      <Person
+      name="batu"
+      email="batu@gmail.com"
+      age={21}
+      isMarried = {false}
+      friends = {["neco","baris","cörüt","albardak"]}
+      />
           
     </div>
     
